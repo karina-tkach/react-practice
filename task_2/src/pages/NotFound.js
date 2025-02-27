355c50d703fd53674bb261dbba7309c8f0c98286
ms.karinatk@gmail.com <ms.karinatk@gmail.com>
2025-02-27 17:45:34 +0200

    Added Not Found page

--- task_2/src/pages/NotFound.js
+++ task_2/src/pages/NotFound.js
@@ -1,10 +1,14 @@
-
 import React from "react";
+import "../assets/css/NotFound.css";
 
-const NotFound = () => {
-    return (
-        <h1>NotFound</h1>
-    );
+export default function NotFoundPage() {
+  return (
+    <div className="not-found-container">
+      <div className="not-found-content">
+        <h1 className="not-found-title">404</h1>
+        <p className="not-found-text">Page Not Found</p>
+        <a href="/" className="not-found-button">Go Home</a>
+      </div>
+    </div>
+  );
 }
-
-export default NotFound;
